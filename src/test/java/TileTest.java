c94f1fd585b10ee4308138fee737adf97103c9a4
Tactee786 <o_ma_c_1997@hotmail.co.uk>
2022-05-18 14:03:45 +0100

    added test class

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TileTest {
+    Tile myTile = new Tile(false, true, false, "O",0);
+
+    @Test
+    public void testGetState(){
+        Assertions.assertEquals("O", myTile.getState(),"incorrect state.");
+    }
+
+}
